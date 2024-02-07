62f3c83360fea19de5e9ee4d24d5ccdc3da59b46
Divyavenkatesan-max <vdivyabharathi68@gmail.com>
2024-02-07 11:36:44 +0530

    new update login

--- src/org/facebook/facebook.java
+++ src/org/facebook/facebook.java
@@ -4,6 +4,9 @@ public class facebook {
 	private void maintest() {
 		System.out.println("browser");
 		System.out.println("url");
+		System.out.println("click login ");
+		System.out.println("username");
+		System.out.println("password");
 		
 
 
