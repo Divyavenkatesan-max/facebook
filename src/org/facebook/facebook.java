4f9cd572b3e248ed1c6334cefaf62ebcfdee10b3
Divyavenkatesan-max <vdivyabharathi68@gmail.com>
2024-02-07 11:21:57 +0530

    update register

--- src/org/facebook/facebook.java
+++ src/org/facebook/facebook.java
@@ -2,12 +2,9 @@ package org.facebook;
 
 public class facebook {
 	private void maintest() {
-		System.out.println("browser");
-
 		System.out.println("maximizee");
 		System.out.println("timeout");
-
-		System.out.println("url");
+		System.out.println("click register");
 		
 
 
